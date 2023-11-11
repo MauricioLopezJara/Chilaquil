4c08fee4dacb57092e25a7d3360f61d7fc6d34bb
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-11-10 18:57:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -87,5 +87,3 @@ elif eleccion == "Datos":
     Datos()
 elif eleccion == "Contacto":
     pagina_contacto()
-# st.sidebar.image("https://universitam.com/academicos/wp-content/uploads/2016/11/reconexion-400x203.jpeg", caption="Descripción de la imagen", use_container_width=True)
-# st.sidebar.image("Reconexion.jpeg")
