13eb430abaf623e820c710652a4443121f4d7dec
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-11-11 22:46:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -46,18 +46,7 @@ def Datos():
     st.title("Diagrama esquimatico")
     st.image("Esquematico.jpeg")
     st.title("Materiales para el circuito:")
-    # Crear un DataFrame de pandas con valores aleatorios
-    # Datos para la tabla
-    datos = {
-        "Componentes": ["Servomotores", "Cables Dupont", "Fuente de alimentación externa", "Arduino Uno"],
-        "Número Componentes necesarios": [5, 16, 1, 1]
-    }
-
-    # Crear un DataFrame de pandas
-    df = pd.DataFrame(datos)
-
-    # Mostrar el DataFrame de manera interactiva
-    st.write(df)
+    
     
 # Función para la página de contacto (Opción 3)
 def pagina_contacto():
