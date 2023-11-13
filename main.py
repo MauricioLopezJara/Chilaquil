4d551cdc4d263df305f8cda1b56fca344895b71e
MauricioLopezJara <94513308+MauricioLopezJara@users.noreply.github.com>
2023-11-13 17:21:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def pagina_bienvenida():
     mid_col.image("Uni.png", width=200)
     st.title("Poster Machine Hand")
     st.write("Este proyecto busca demostrar el poder que se puede generar con un poco de conocimiento en programacion al igual que en electronica")
-    st.header("Ayudame a llenar este pedo")
+    st.header("Fase Inicial del proyecto")
     st.write("Ayudame a llenar este pedo")
     st.write("Ayudame a llenar este pedo")
         # URL del video de YouTube
